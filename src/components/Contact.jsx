00ff70e7878a95ea79a321c4d21d61c68d689d9b
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2025-02-03 23:02:18 +0500

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,25 @@
+import axios from 'axios';
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
+
+// Getform.com   Use message recive me
 function Contact() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async (data) => {
+       const userInfo = {
+        name: data.name,
+        email: data.email,
+        message: data.message
+       }
+    try {
+       await  axios.post('https://getform.io/f/bejjqjxa', userInfo);
+       alert('Message sent successfully!');
+    } catch (error) {
+        console.log(error);
+        
+    }
+    } 
     return (
         <>
             <div name='Contact' className='max-w-screen-2xl mx-auto container px-4 md:px-20 my-16'>
@@ -12,14 +31,20 @@ function Contact() {
                         <div class="mx-auto">
                             <div class="max-w-md mx-auto px-8 py-6 bg-gray-100 rounded-lg shadow-lg">
                                 <h2 class="text-2xl font-semibold text-gray-800 mb-4">Contact Us</h2>
-                                <form>
+                                <form 
+                                    // action='https://getform.io/f/bejjqjxa'
+                                    // method='POST' 
+                                    onSubmit={handleSubmit(onSubmit)}>
                                     <div class="mb-4">
                                         <label class="block text-gray-800 mb-1" for="name">Your Name</label>
                                         <input
                                             class="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
                                             placeholder="Enter your name"
                                             type="text"
+                                            name='name'
+                                            {...register("name", { required: true })}
                                         />
+                                        {errors.name && <span>This field is required</span>}
                                     </div>
                                     <div class="mb-4">
                                         <label class="block text-gray-800 mb-1" for="email">Your Email</label>
@@ -29,19 +54,22 @@ function Contact() {
                                             name="email"
                                             id="email"
                                             type="email"
+                                            {...register("email", { required: true })}
                                         />
+                                        {errors.email && <span>This field is required</span>}
                                     </div>
                                     <div class="mb-4">
                                         <label class="block text-gray-800 mb-1" for="message"
-                                        >Your Message</label
-                                        >
+                                        >Your Message</label>
                                         <textarea
                                             class="w-full px-4 py-2 bg-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-300 transition duration-300"
                                             rows="4"
                                             placeholder="Enter your message"
                                             name="message"
                                             id="message"
+                                            {...register("message", { required: true })}
                                         ></textarea>
+                                        {errors.message && <span>This field is required</span>}
                                     </div>
                                     <button
                                         class="w-full bg-yellow-300 text-gray-800 py-2 px-4 rounded-lg hover:bg-yellow-400 transition duration-300"

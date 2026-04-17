c3097ab3d344d213294a1dcb6a42f847db3834fe
Hammad Liaquat <154601362+hammadliaquat77@users.noreply.github.com>
2026-04-17 20:22:43 +0500

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,116 @@
+// import React from 'react'
+// import { FaFacebook } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa";
+// import { FaGithub } from "react-icons/fa";
+// import { FaInstagramSquare } from "react-icons/fa";
+// import { FaReact } from "react-icons/fa";
+// import { SiMongodb } from "react-icons/si";
+// import { SiExpress } from "react-icons/si";
+// import { FaNodeJs } from "react-icons/fa";
+// import { ReactTyped } from "react-typed";
+
+// // import pic from '../assets/Hammad.jpg';
+// import Hammad from '../assets/HammadSocial.jpg';
+
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+
+
+// function Home() {
+
+//     AOS.init({
+//         duration: 1000,
+//         once: false,
+//         mirror: true,
+//     });
+
+//     return (
+//         <>
+//             <div name='Home' className='max-w-screen-2xl mx-auto container px-4 md:px-20 py-20 dark:bg-gray-900 dark:text-white'>
+//                 <div className='flex flex-col md:flex-row'>
+//                     <div data-aos="fade-right" className='md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1'>
+//                         <span className='text-xl'>Welcome In My PortFolio</span>
+//                         <div className='flex space-x-1 text-2xl md:text-4xl'>
+//                             <h1>Hello, I'm a</h1>
+//                             {/* <span className='text-red-700 font-bold '>Developer</span> */}
+//                             <ReactTyped
+//                                 className='text-red-700 font-bold'
+//                                 strings={["Developer", "Programer", "Coder"]}
+//                                 typeSpeed={40}
+//                                 backSpeed={50}
+//                                 loop={true}
+//                             />
+
+//                         </div>
+//                         <br />
+//                         <p className='text-sm md:text-xl text-justify'>
+//                             My name is Hammad Liaquat, and I am a MERN Stack Developer with a background in BSc Computer Science. I have started my career as a MERN Stack Developer, working with core technologies including MongoDB, Express.js, React.js, and Node.js to build dynamic and scalable web applications.
+//                         </p>
+//                         <br />
+
+//                         {/*Social Media Icons  */}
+//                         <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+//                             <div className="  space-y-2">
+//                                 <h1 className="font-bold text-center ">Available on</h1>
+//                                 <ul className="flex space-x-5">
+//                                     <li>
+//                                         <a href="https://www.facebook.com/share/1Dj9eMQEZv/" target="_blank">
+//                                             <FaFacebook className="text-2xl cursor-pointer" target="_blank" />
+//                                         </a>
+//                                     </li>
+//                                     <li>
+//                                         <a href="https://www.linkedin.com/in/hammad-raza77/" target="_blank">
+//                                             <FaLinkedin className="text-2xl cursor-pointer" />
+//                                         </a>
+//                                     </li>
+//                                     <li>
+//                                         <a href="https://www.instagram.com/" target="_blank">
+//                                             <FaInstagramSquare className="text-2xl cursor-pointer" />
+//                                         </a>
+//                                     </li>
+//                                     <li>
+//                                         <a href="https://github.com/hammadliaquat77" target="_blank">
+//                                             <FaGithub className="text-2xl cursor-pointer" />
+//                                         </a>
+//                                     </li>
+//                                 </ul>
+//                             </div>
+//                             <div className=" space-y-2">
+//                                 <h1 className="font-bold text-center">Currently working on</h1>
+//                                 <div className="flex space-x-5">
+//                                     <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+//                                     <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+//                                     <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+//                                     <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+//                                 </div>
+//                             </div>
+//                         </div>
+
+
+//                     </div>
+//                     {/* Image */}
+//                     <div data-aos="fade-left" className='md:w-1/2  md:ml-48 md:mt-20 order-1 mt-8'>
+//                         <img src={Hammad} alt="" className='rounded-full md:h-[450px] md:w-[450px]' />
+//                     </div>
+//                 </div>
+//             </div>
+//             <hr />
+//         </>
+//     )
+// }
+
+// export default Home
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -9,13 +122,10 @@ import { SiExpress } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { ReactTyped } from "react-typed";
 
-// import pic from '../assets/Hammad.jpg';
 import Hammad from '../assets/HammadSocial.jpg';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
-
 
 function Home() {
 
@@ -29,46 +139,74 @@ function Home() {
         <>
             <div name='Home' className='max-w-screen-2xl mx-auto container px-4 md:px-20 py-20 dark:bg-gray-900 dark:text-white'>
                 <div className='flex flex-col md:flex-row'>
+
+                    {/* LEFT SIDE */}
                     <div data-aos="fade-right" className='md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1'>
                         <span className='text-xl'>Welcome In My PortFolio</span>
+
                         <div className='flex space-x-1 text-2xl md:text-4xl'>
                             <h1>Hello, I'm a</h1>
-                            {/* <span className='text-red-700 font-bold '>Developer</span> */}
+
                             <ReactTyped
                                 className='text-red-700 font-bold'
-                                strings={["Developer", "Programer", "Coder"]}
+                                strings={["Developer", "Programmer", "Coder"]}
                                 typeSpeed={40}
                                 backSpeed={50}
                                 loop={true}
                             />
-
                         </div>
-                        <br />
-                        <p className='text-sm md:text-xl text-justify'>
-                            My name is Hammad Liaquat, and I am a MERN Stack Developer with a background in BSc Computer Science. I have started my career as a MERN Stack Developer, working with core technologies including MongoDB, Express.js, React.js, and Node.js to build dynamic and scalable web applications.
-                        </p>
+
                         <br />
 
-                        {/*Social Media Icons  */}
-                        <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
-                            <div className="  space-y-2">
-                                <h1 className="font-bold text-center ">Available on</h1>
+                        <p className='text-sm md:text-xl text-justify'>
+                            My name is Hammad Liaquat, and I am a MERN Stack Developer with a background in BSc Computer Science. I build dynamic and scalable web applications using MongoDB, Express.js, React.js, and Node.js.
+                        </p>
+
+                        <br />
+
+                        {/* BUTTONS */}
+                        <div className="flex space-x-4 pt-4">
+                            {/* <a
+                                href="/Hammad_Resume.pdf"
+                                download
+                                className="bg-red-600 text-white px-5 py-2 rounded-lg hover:bg-red-700 duration-200"
+                            >
+                                Download Resume
+                            </a> */}
+
+                           
+                            <button
+                                onClick={() => window.open("/hammad_raza_mernstack.pdf", "_blank")}
+                                className="border border-red-600 text-red-600 px-5 py-2 rounded-lg hover:bg-red-600 hover:text-white duration-200"
+                            >
+                                Hire Me
+                            </button>
+                        </div>
+
+                        {/* SOCIAL MEDIA */}
+                        <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0 pt-6">
+
+                            <div className="space-y-2">
+                                <h1 className="font-bold text-center">Available on</h1>
                                 <ul className="flex space-x-5">
                                     <li>
                                         <a href="https://www.facebook.com/share/1Dj9eMQEZv/" target="_blank">
-                                            <FaFacebook className="text-2xl cursor-pointer" target="_blank" />
+                                            <FaFacebook className="text-2xl cursor-pointer" />
                                         </a>
                                     </li>
+
                                     <li>
                                         <a href="https://www.linkedin.com/in/hammad-raza77/" target="_blank">
                                             <FaLinkedin className="text-2xl cursor-pointer" />
                                         </a>
                                     </li>
+
                                     <li>
                                         <a href="https://www.instagram.com/" target="_blank">
                                             <FaInstagramSquare className="text-2xl cursor-pointer" />
                                         </a>
                                     </li>
+
                                     <li>
                                         <a href="https://github.com/hammadliaquat77" target="_blank">
                                             <FaGithub className="text-2xl cursor-pointer" />
@@ -76,25 +214,27 @@ function Home() {
                                     </li>
                                 </ul>
                             </div>
-                            <div className=" space-y-2">
+
+                            <div className="space-y-2">
                                 <h1 className="font-bold text-center">Currently working on</h1>
                                 <div className="flex space-x-5">
-                                    <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
-                                    <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                                    <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px]" />
+                                    <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px]" />
+                                    <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px]" />
+                                    <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px]" />
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
-                    {/* Image */}
-                    <div data-aos="fade-left" className='md:w-1/2  md:ml-48 md:mt-20 order-1 mt-8'>
+
+                    {/* RIGHT SIDE IMAGE */}
+                    <div data-aos="fade-left" className='md:w-1/2 md:ml-48 md:mt-20 order-1 mt-8'>
                         <img src={Hammad} alt="" className='rounded-full md:h-[450px] md:w-[450px]' />
                     </div>
+
                 </div>
             </div>
+
             <hr />
         </>
     )
